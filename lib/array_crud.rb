5790b49fefeff30744699541492eace62fa529ee
Sarah <sarahlchavez@gmail.com>
2021-02-26 20:57:50 -0800

    commit tree 55037c677a917922ac78abca0926bde34ac13e8a

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,53 @@
 def create_an_empty_array
-  
+    
+    []
+
 end
 
 def create_an_array
-  
+    
+    ["Jerry", "Kramer", "George", "Elaine"] 
+
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    
+    array << "arrays!"    
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    
+    array.unshift("wow")  
+    
 end
 
 def remove_element_from_end_of_array(array)
   
+    last = array.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+    
+    first = array.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
+    array[2]
+
 end
 
 def retrieve_first_element_from_array(array)
   
+    array.first
+
 end
 
 def retrieve_last_element_from_array(array)
   
+    array.last
+
 end
